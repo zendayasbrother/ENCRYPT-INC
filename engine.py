cb08775130431beb4ca7f0a655db77d918dddf30
Daniel Onyeakazi <126322853+zendayasbrother@users.noreply.github.com>
2026-04-13 23:28:32 +0100

    Update engine.py - move proposals pop up here

--- engine.py
+++ engine.py
@@ -11,14 +11,28 @@ class ManagementSystem(AuthSystem, DataManager):
         # Initialize both parent classes
         AuthSystem.__init__(self, db_path)
         DataManager.__init__(self, db_path)
+        
+    def proposals(self):
+        conn, cursor = self.connect_database()
+        if cursor:
+            try:
+                cursor.execute("SELECT * FROM proposals")
+                return cursor.fetchall()
+            except Exception as e:
+                print(f"Error fetching proposals: {e}")
+            finally:
+                conn.close()
+        return [] # first, admin window, then talent inputs and requests
     
     def run_app(self):
         print("--- Encrypt Inc. OS ---")
         user = input("Username: ")
         pw = input("Password: ")
         
+        
         if self.authenticate_user(user, pw):
             print("\nLogin Successful!")
+            self.proposals() 
             self.display_table_interface()
         else:
             print("\nAccess Denied.")
