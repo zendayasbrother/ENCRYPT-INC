4fbbd78aeaa93223b0921406d55ddbef03efec36
Daniel Onyeakazi <do4009@hw.ac.uk>
2026-07-30 14:16:10 +0100

    finalise migration for v0

--- core/urls.py
+++ core/urls.py
@@ -1,1 +1,10 @@
+# encrypt_inc/apps/core/urls.py
+from django.urls import path
+from .views import ProposalSubmitView, ProposalQueueView
+
 # API Routing Table
+
+urlpatterns = [
+    path('proposals/submit/', ProposalSubmitView.as_view(), name='proposal-submit'),
+    path('proposals/queue/', ProposalQueueView.as_view(), name='proposal-queue'),
+]
